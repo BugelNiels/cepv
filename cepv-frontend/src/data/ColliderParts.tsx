78ee8adb28775dd3481d1a346cfd2ff908c3fb48
niels bagel <bugel.niels@gmail.com>
2024-01-18 10:40:39 +0100

    Updated collider part colors

--- cepv-frontend/src/data/ColliderParts.tsx
+++ cepv-frontend/src/data/ColliderParts.tsx
@@ -35,139 +35,139 @@ interface ColliderParts {
 const defaultColliderParts: ColliderParts = {
     beamPipe: {
         name: "Beam Pipe",
-        color: "#6f7f7f",
+        color: "#BBBBBB",
         enabled: true,
         path: "/models/Beam_Pipe.glb"
     },
     cathodeStripChambers: {
         name: "Cathode Strip Chambers",
-        color: "#000000",
+        color: "#F4F4F4",
         enabled: true,
         path: "/models/Cathode_Strip_Chambers.glb"
     },
     driftTubes: {
         name: "Drift Tubes",
-        color: "#000000",
+        color: "#F4F4F4",
         enabled: true,
         path: "/models/Drift_Tubes.glb"
     },
     ecalBarrel: {
         name: "ECAL Barrel",
-        color: "#000000",
+        color: "#AAFFE7",
         enabled: true,
         path: "/models/ECAL_Barrel.glb"
     },
     ecalEndcapNeg: {
         name: "ECAL Endcap (-)",
-        color: "#000000",
+        color: "#AAFFE7",
         enabled: true,
         path: "/models/ECAL_Endcap_neg.glb"
     },
     ecalEndcapPos: {
         name: "ECAL Endcap (+)",
-        color: "#000000",
+        color: "#AAFFE7",
         enabled: true,
         path: "/models/ECAL_Endcap_pos.glb"
     },
     hcalBarrel: {
         name: "HCAL Barrel",
-        color: "#000000",
+        color: "#FFE7AA",
         enabled: true,
         path: "/models/HCAL_Barrel.glb"
     },
     hcalEndcapNeg: {
         name: "HCAL Endcap (-)",
-        color: "#000000",
+        color: "#FFE7AA",
         enabled: true,
         path: "/models/HCAL_Endcap_neg.glb"
     },
     hcalEndcapPos: {
         name: "HCAL Endcap (+)",
-        color: "#000000",
+        color: "#FFE7AA",
         enabled: true,
         path: "/models/HCAL_Endcap_pos.glb"
     },
     hcalFordwardNeg: {
         name: "HCAL Forward (-)",
-        color: "#f3a011",
+        color: "#FFBC00",
         enabled: true,
         path: "/models/HCAL_Forward_neg.glb"
     },
     hcalFordwardPos: {
         name: "HCAL Forward (+)",
-        color: "#f3a011",
+        color: "#FFBC00",
         enabled: true,
         path: "/models/HCAL_Forward_pos.glb"
     },
     hcalOuter: {
         name: "HCAL Outer",
-        color: "#000000",
+        color: "#F4F4F4",
         enabled: true,
         path: "/models/HCAL_Outer.glb"
     },
     pixelBarrel: {
         name: "Pixel Barrel",
-        color: "#000000",
+        color: "#E7FFAA",
         enabled: true,
         path: "/models/Pixel_Barrel.glb"
     },
     pixelEndcapNeg: {
         name: "Pixel Endcap (-)",
-        color: "#000000",
+        color: "#E7FFAA",
         enabled: true,
         path: "/models/Pixel_Endcap_neg.glb"
     },
     pixelEndcapPos: {
         name: "Pixel Endcap (+)",
-        color: "#000000",
+        color: "#E7FFAA",
         enabled: true,
         path: "/models/Pixel_Endcap_pos.glb"
     },
     steelYoke: {
         name: "Steel Yoke",
-        color: "#f71500",
+        color: "#FF0000",
         enabled: true,
         path: "/models/Steel_Yoke.glb"
     },
     support: {
         name: "Support",
-        color: "#f71500",
+        color: "#FF0000",
         enabled: true,
         path: "/models/Support.glb"
     },
     trackerEndcapNeg: {
         name: "Tracker Endcap (-)",
-        color: "#e9ece9",
+        color: "#AAE7FF",
         enabled: true,
         path: "/models/Tracker_Endcap_neg.glb"
     },
     trackerEndcapPos: {
         name: "Tracker Endcap (+)",
-        color: "#e9ece9",
+        color: "#AAE7FF",
         enabled: true,
         path: "/models/Tracker_Endcap_pos.glb"
     },
     trackerInnerBarrel: {
         name: "Tracker Inner Barrel",
-        color: "#000000",
+        color: "#AAE7FF",
         enabled: true,
         path: "/models/Tracker_Inner_Barrel.glb"
     },
     trackerInnerBarrelNeg: {
         name: "Tracker Inner Barrel (-)",
-        color: "#000000",
+        color: "#AAE7FF",
         enabled: true,
         path: "/models/Tracker_Inner_Barrel_neg.glb"
     },
     trackerInnerBarrelPos: {
         name: "Tracker Inner Barrel (+)",
-        color: "#000000",
+        color: "#AAE7FF",
         enabled: true,
         path: "/models/Tracker_Inner_Barrel_pos.glb"
     },
     trackerOuterBarrel: {
         name: "Tracker Outer Barrel",
-        color: "#000000",
+        color: "#AAE7FF",
         enabled: true,
         path: "/models/Tracker_Outer_Barrel.glb"
     },
